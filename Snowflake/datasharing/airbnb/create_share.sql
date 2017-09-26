58269389d82cb99d3b437abfdf767de86195e1bb
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 16:37:25 -0400

    Create create_share.sql

---
+++ Snowflake/datasharing/airbnb/create_share.sql
@@ -0,0 +1,4 @@
+create share airbnb_crm_data;
+grant usage on database data_sharing_air to share airbnb_crm_data;
+grant usage on schema data_sharing_air.shared to share airbnb_crm_data;
+grant select on view data_sharing_air.shared.vw_crm_data to share airbnb_crm_data; 
