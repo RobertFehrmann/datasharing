cc99c9dad67bf52945f451d523e397c781fc6531
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 11:39:27 -0400

    Create create_share.sql

---
+++ Snowflake/datasharing/create_share.sql
@@ -0,0 +1,4 @@
+create share cardlytics_campaign_data;
+grant usage on database data_sharing_cl to share cardlytics_campaign_data;
+grant usage on schema data_sharing_cl.cardlytics to share cardlytics_campaign_data;
+grant select on table data_sharing_cl.cardlytics.campaign_data to share cardlytics_campaign_data; 
