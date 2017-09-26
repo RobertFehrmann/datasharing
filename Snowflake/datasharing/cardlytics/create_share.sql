56f68e3d959401366b42f1a21670f039396b49c2
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 17:57:01 -0400

    Update create_share.sql

--- Snowflake/datasharing/cardlytics/create_share.sql
+++ Snowflake/datasharing/cardlytics/create_share.sql
@@ -1,6 +1,6 @@
 DROP SHARE IF EXISTS cardlytics_campaign_data;
 CREATE SHARE cardlytics_campaign_data;
 GRANT USAGE ON DATABASE data_sharing_cl TO SHARE cardlytics_campaign_data;
-GRANT USAGE ON SCHEMA data_sharing_cl.shared TO SHARE cardlytics_campaign_data;
-GRANT SELECT ON VIEW data_sharing_cl.shared.vw_campaign_data TO SHARE cardlytics_campaign_data; 
+USE DATABASE data_sharing_cl;
+GRANT USAGE ON SCHEMA shared TO SHARE cardlytics_campaign_data;
 ALTER SHARE cardlytics_campaign_data ADD accounts=pm2
