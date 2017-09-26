1dc41c782207f83acf79541a1b078a0528e071ec
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 18:11:55 -0400

    Update insert_crm_data.sql

--- Snowflake/datasharing/airbnb/insert_crm_data.sql
+++ Snowflake/datasharing/airbnb/insert_crm_data.sql
@@ -1,4 +1,7 @@
-insert into data_sharing_air.airbnb.crm_data values (5,135.45,'ABC123');
-insert into data_sharing_air.airbnb.crm_data values (4,650.47,'BCD234');
-insert into data_sharing_air.airbnb.crm_data values (5,126.53,'CDE345');
-insert into data_sharing_air.airbnb.crm_data values (1,456.15,'DEF456');
+USE DATABASE data_sharing_air;
+USE SCHEMA airbnb;
+TRUNCATE TABLE crm_data;
+INSERT INTO crm_data VALUES (5,135.45,'ABC123');
+INSERT INTO crm_data VALUES (4,650.47,'BCD234');
+INSERT INTO crm_data VALUES (5,126.53,'CDE345');
+INSERT INTO crm_data VALUES (1,456.15,'DEF456');
