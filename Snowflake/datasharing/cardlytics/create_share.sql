6d6c6acb920d58ae80bd0519cdbc0e795f2d3c46
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 16:39:50 -0400

    Update create_share.sql

--- Snowflake/datasharing/cardlytics/create_share.sql
+++ Snowflake/datasharing/cardlytics/create_share.sql
@@ -2,3 +2,4 @@ create share cardlytics_campaign_data;
 grant usage on database data_sharing_cl to share cardlytics_campaign_data;
 grant usage on schema data_sharing_cl.cardlytics to share cardlytics_campaign_data;
 grant select on table data_sharing_cl.cardlytics.campaign_data to share cardlytics_campaign_data; 
+alter share cardlytics_campaign_data add accounts=pm2
