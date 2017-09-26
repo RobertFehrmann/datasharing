792ede0ce48b593f9acc006bbce66fd03c689e9c
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 17:47:14 -0400

    Update insert_campaign_data.sql

--- Snowflake/datasharing/cardlytics/insert_campaign_data.sql
+++ Snowflake/datasharing/cardlytics/insert_campaign_data.sql
@@ -1,4 +1,4 @@
-insert into data_sharing_cl.cardlytics.campaign_data values ('ABC123','New',to_date('9/17/2017'),135.45);
-insert into data_sharing_cl.cardlytics.campaign_data values ('BCD234','Frequent',to_date('9/18/2017'),110.93);
-insert into data_sharing_cl.cardlytics.campaign_data values ('CDE345','Infrequent',to_date('9/19/2017'),75.81);
-insert into data_sharing_cl.cardlytics.campaign_data values ('DEF456','New',to_date('9/20/2017'),456.15);
+INSERT INTO cardlytics.campaign_data VALUES ('ABC123','New',to_date('9/17/2017'),135.45);
+INSERT INTO cardlytics.campaign_data VALUES ('BCD234','Frequent',to_date('9/18/2017'),110.93);
+INSERT INTO ardlytics.campaign_data VALUES ('CDE345','Infrequent',to_date('9/19/2017'),75.81);
+INSERT INTO cardlytics.campaign_data VALUES ('DEF456','New',to_date('9/20/2017'),456.15);
