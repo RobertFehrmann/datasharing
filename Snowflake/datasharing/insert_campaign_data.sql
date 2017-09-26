31591e83287bea11f7c286720790d797100f75e6
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 11:23:46 -0400

    Create insert_campaign_data.sql

---
+++ Snowflake/datasharing/insert_campaign_data.sql
@@ -0,0 +1,4 @@
+insert into data_sharing_cl.cardlytics.campaign_data values ('ABC123','New',to_date('9/17/2017'),135.45);
+insert into data_sharing_cl.cardlytics.campaign_data values ('BCD234','Frequent',to_date('9/18/2017'),110.93);
+insert into data_sharing_cl.cardlytics.campaign_data values ('CDE345','Infrequent',to_date('9/19/2017'),75.81);
+insert into data_sharing_cl.cardlytics.campaign_data values ('DEF456','New',to_date('9/20/2017'),456.15);
