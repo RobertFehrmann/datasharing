e77ad3e5e1bf9c2a4b811e2b61a454ba18f65992
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 16:48:44 -0400

    Create insert_crm_data.sql

---
+++ Snowflake/datasharing/airbnb/insert_crm_data.sql
@@ -0,0 +1,4 @@
+insert into data_sharing_air.airbnb.crm_data values (5,135.45,'ABC123');
+insert into data_sharing_air.airbnb.crm_data values (4,650.47,'BCD234');
+insert into data_sharing_air.airbnb.crm_data values (5,126.53,'CDE345');
+insert into data_sharing_air.airbnb.crm_data values (1,456.15,'DEF456');
