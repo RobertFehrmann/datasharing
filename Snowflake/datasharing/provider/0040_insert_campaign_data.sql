550c210beb335f0bb1e99b406262d832a38b2d1b
RobertFehrmann <RobertFehrmann2906@gmail.com>
2017-09-26 21:51:17 -0400

    Update and rename Snowflake/datasharing/cardlytics/004_insert_campaign_data.sql to Snowflake/datasharing/provider/0040_insert_campaign_data.sql

--- Snowflake/datasharing/provider/0040_insert_campaign_data.sql
+++ Snowflake/datasharing/provider/0040_insert_campaign_data.sql
@@ -1,5 +1,5 @@
-USE DATABASE DATA_SHARING_CL;
-USE SCHEMA cardlytics;
+USE DATABASE DATA_SHARING;
+USE SCHEMA FACTS;
 TRUNCATE TABLE campaign_data;
 INSERT INTO campaign_data VALUES ('ABC123','New',to_date('9/17/2017'),135.45);
 INSERT INTO campaign_data VALUES ('BCD234','Frequent',to_date('9/18/2017'),110.93);
